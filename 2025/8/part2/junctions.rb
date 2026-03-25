39a559e5b34e02378525f3bb44a2c842627ee1cd
Jason Uhlenkott <jpu1012@uhlenkott.net>
2026-03-25 22:40:03 +0000

    day 8 part 2: use Prim's MST algorithm

--- 2025/8/part2/junctions.rb
+++ 2025/8/part2/junctions.rb
@@ -1,71 +1,58 @@
 #!/usr/bin/env ruby
 
-def distance(box, other)
+# Prim's MST algorithm — O(N²) time, O(N) memory.
+
+def distance_sq(box, other)
   x = box[0] - other[0]
   y = box[1] - other[1]
   z = box[2] - other[2]
-
-  Math.sqrt(x**2 + y**2 + z**2)
+  x * x + y * y + z * z
 end
 
 def main
-  a=[]
   f = open("input.txt")
   boxes = []
   f.each_line do |line|
     boxes.push(line.split(",").map { |val| val.to_i })
   end
 
-  distances = []
-  boxes.each_with_index do |box, i|
-    boxes[i+1..].each do |other|
-      distances.push([distance(box, other), box, other])
-    end
-  end
+  n = boxes.size
+  in_mst = Array.new(n, false)
+  min_dist = Array.new(n, Float::INFINITY)
+  min_from = Array.new(n, -1)
 
-  distances.sort!
-  #distances.each { |d| p d }
+  # Start from vertex 0.
+  min_dist[0] = 0
+  last_u = nil
+  last_from = nil
 
-  circuits = {}
-  circuit_count = 0
-  box_circuits = {}
-
-  distances.each do |d|
-    if box_circuits[d[1]].nil? && box_circuits[d[2]].nil?
-      circid = circuits.size
-      circuits[circid] = [d[1], d[2]]
-      circuit_count += 1
-      box_circuits[d[1]] = circid
-      box_circuits[d[2]] = circid
-    elsif box_circuits[d[1]] == box_circuits[d[2]]
-      # Already in same circuit.  Nothing happens.
-    elsif !box_circuits[d[1]].nil? && !box_circuits[d[2]].nil? && box_circuits[d[1]] != box_circuits[d[2]]
-      # Joining two different circuits.
-      circ = box_circuits[d[1]]
-      othercirc = box_circuits[d[2]]
-      circuits[othercirc].each do |box|
-        box_circuits[box] = circ
+  n.times do
+    # Find the unvisited vertex with smallest min_dist.
+    u = nil
+    best = Float::INFINITY
+    n.times do |v|
+      if !in_mst[v] && min_dist[v] < best
+        best = min_dist[v]
+        u = v
       end
-      circuits[circ] += circuits[othercirc]
-      circuits[othercirc] = []
-      circuit_count -= 1
-    elsif !box_circuits[d[1]].nil? && box_circuits[d[2]].nil?
-      circ = box_circuits[d[1]]
-      box_circuits[d[2]] = circ
-      circuits[circ].push(d[2])
-    elsif box_circuits[d[1]].nil? && !box_circuits[d[2]].nil?
-      circ = box_circuits[d[2]]
-      box_circuits[d[1]] = circ
-      circuits[circ].push(d[1])
-    else
-      fail "wtf"
     end
 
-    if box_circuits.size == boxes.size && circuit_count == 1
-      p "All connected after joining #{d}.  x product is #{d[1][0] * d[2][0]}."
-      return
+    in_mst[u] = true
+    last_u = u
+    last_from = min_from[u]
+
+    # Update distances to remaining vertices.
+    n.times do |v|
+      next if in_mst[v]
+      d = distance_sq(boxes[u], boxes[v])
+      if d < min_dist[v]
+        min_dist[v] = d
+        min_from[v] = u
+      end
     end
   end
+
+  p "All connected, x product is #{boxes[last_from][0] * boxes[last_u][0]}."
 end
 
 main()
